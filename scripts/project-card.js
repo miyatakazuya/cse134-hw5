1c71ae2b072797edffda0ed86e223b32f90ae141
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2025-03-15 18:23:43 -0700

    feat: Part 1 Completion; Added custom attributes to handle content in project-card

--- scripts/project-card.js
+++ scripts/project-card.js
@@ -23,13 +23,27 @@ class ProjectCardComponent extends HTMLElement {
         // Picture <picture>
         this.projectPicture = document.createElement('picture');
 
+        let defaultImage;
+        if (this.hasAttribute('data-image-default')) {
+            defaultImage = this.getAttribute('data-image-default');
+        } else {
+            defaultImage = "../images/default-featured-image.jpg";
+        }
+
         this.source = document.createElement('source');
-        this.source.setAttribute('srcset','../images/toothless.jpg');
+        this.source.setAttribute('srcset', defaultImage);
         this.source.setAttribute('media','(min-width: 700px)');
         this.source.setAttribute('type','image/jpg');
 
+        let altImage;
+        if (this.hasAttribute('data-image-alt')) {
+            altImage = this.getAttribute('data-image-alt');
+        } else {
+            altImage = "../images/default-featured-image.jpg";
+        }
+
         this.image = document.createElement('img');
-        this.image.setAttribute('src', '../images/toothless_small.png');
+        this.image.setAttribute('src', altImage);
         this.image.setAttribute('width', '1000');
         this.image.setAttribute('alt', 'Toothless AUV');
         
@@ -44,13 +58,32 @@ class ProjectCardComponent extends HTMLElement {
 
         // Header <h2>
         this.projectHeader = document.createElement('h3');
-        this.projectHeader.textContent = 'Project name';
+
+        let headerText;
+        if (this.hasAttribute('data-header')) {
+            headerText = this.getAttribute('data-header');
+        } else {
+            headerText = "Default Project";
+        }
+        this.projectHeader.textContent = headerText;
         this.info.appendChild(this.projectHeader);
         
         // Description <p>
-        this.description = document.createElement('p');
-        this.description.textContent = 'Short Project Description';
-        this.info.appendChild(this.description);
+
+        this.description = this.querySelector('.description');
+        if (this.description) {
+            this.removeChild(this.description);
+            this.info.appendChild(this.description);
+        } else {
+            this.description = document.createElement('p');
+            this.description.textContent = 'Short Project Description';
+            this.info.appendChild(this.description);
+        }
+
+        // this.description = document.createElement('p');
+        // this.description.setAttribute('name', 'description');
+        // this.description.textContent = 'Short Project Description';
+        // this.info.appendChild(this.description);    
 
         // Used Skills <div>
         this.skills = document.createElement('div');
@@ -59,8 +92,14 @@ class ProjectCardComponent extends HTMLElement {
 
         // More Info <a>
         this.hyperlink = document.createElement('a');
-        this.hyperlink.setAttribute('href', 'https://github.com/miyatakazuya');
-        this.hyperlink.textContent = 'more info';
+        let hyperlinkText;
+        if (this.hasAttribute('data-hyperlink')) {
+            hyperlinkText = this.getAttribute('data-hyperlink');
+        } else {
+            hyperlinkText = 'https://github.com/miyatakazuya';
+        }
+        this.hyperlink.setAttribute('href', hyperlinkText);
+        this.hyperlink.textContent = 'More Information Here';
         this.info.appendChild(this.hyperlink);
     }
 }
